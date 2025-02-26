ea9159b588e4235979f53738cea52a327409a489
hest <henrik.starefors@gmail.com>
2025-02-26 15:03:30 +0100

    reverse section a colors

--- lua/lualine/themes/nordic.lua
+++ lua/lualine/themes/nordic.lua
@@ -3,37 +3,37 @@ local C = require('nordic.colors')
 local nordic = {}
 
 nordic.normal = {
-    a = { bg = C.orange.bright, fg = C.black0, gui = 'bold' },
+    a = { bg = C.black0, fg = C.orange.bright, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white0 },
 }
 
 nordic.insert = {
-    a = { bg = C.green.bright, fg = C.black0, gui = 'bold' },
+    a = { bg = C.black0, fg = C.green.bright, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white0 },
 }
 
 nordic.command = {
-    a = { bg = C.cyan.bright, fg = C.black0, gui = 'bold' },
+    a = { bg = C.black0, fg = C.cyan.bright, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white0 },
 }
 
 nordic.visual = {
-    a = { bg = C.red.bright, fg = C.black0, gui = 'bold' },
+    a = { bg = C.black0, fg = C.red.bright, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white0 },
 }
 
 nordic.replace = {
-    a = { bg = C.magenta.bright, fg = C.black0, gui = 'bold' },
+    a = { bg = C.black0, fg = C.magenta.bright, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white0 },
 }
 
 nordic.terminal = {
-    a = { bg = C.blue2, fg = C.black0, gui = 'bold' },
+    a = { bg = C.black0, fg = C.blue2, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
     c = { bg = C.bg_statusline, fg = C.white0 },
 }
